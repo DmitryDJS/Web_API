3e2d94c1e96561d28d6f01e61cec2638c090a8a8
Leks99 <lekssd@yandex.ru>
2022-12-13 18:35:29 +0300

    working with the response dictionary

--- python_repos.py
+++ python_repos.py
@@ -9,3 +9,14 @@ print("Статус код:", r.status_code)
 response_dict = r.json()
 
 print("Общее количество репозиториев:", response_dict['total_count'])
+
+# Analysis of repository information
+repo_dicts = response_dict['items']
+print("Возвращенные репозитории:", len(repo_dicts))
+
+# Analysis first repository
+repo_dict = repo_dicts[0]
+print("\nKeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
+
